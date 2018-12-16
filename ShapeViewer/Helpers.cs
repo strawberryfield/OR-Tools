0326d2399a17a4cc5799a7b6f736cb6360bfd31a
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2018-12-16 18:44:36 +0100

    Model rendering

---
+++ ShapeViewer/Helpers.cs
@@ -0,0 +1,239 @@
+﻿// COPYRIGHT 2010, 2011, 2012 by the Open Rails project.
+// 
+// This file is part of Open Rails.
+// 
+// Open Rails is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Open Rails is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Open Rails.  If not, see <http://www.gnu.org/licenses/>.
+
+using Orts.Formats.Msts;
+using ORTS.Settings;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Orts.Viewer3D.Common
+{
+    public class Simulator {
+        public readonly UserSettings Settings;
+
+        public string BasePath;     // ie c:\program files\microsoft games\train simulator
+        public string RoutePath;    // ie c:\program files\microsoft games\train simulator\routes\usa1  - may be different on different pc's
+
+        public SeasonType Season;
+        public WeatherType WeatherType;
+
+    }
+
+    public static class Helpers
+    {
+        [Flags]
+        public enum TextureFlags
+        {
+            None = 0x0,
+            Snow = 0x1,
+            SnowTrack = 0x2,
+            Spring = 0x4,
+            Autumn = 0x8,
+            Winter = 0x10,
+            SpringSnow = 0x20,
+            AutumnSnow = 0x40,
+            WinterSnow = 0x80,
+            Night = 0x100,
+            Underground = 0x40000000,
+        }
+
+        [Flags]
+        public enum SceneryMaterialOptions
+        {
+            None = 0,
+            // Diffuse
+            Diffuse = 0x1,
+            // Alpha test
+            AlphaTest = 0x2,
+            // Blending
+            AlphaBlendingNone = 0x0,
+            AlphaBlendingBlend = 0x4,
+            AlphaBlendingAdd = 0x8,
+            AlphaBlendingMask = 0xC,
+            // Shader
+            ShaderImage = 0x00,
+            ShaderDarkShade = 0x10,
+            ShaderHalfBright = 0x20,
+            ShaderFullBright = 0x30,
+            ShaderVegetation = 0x40,
+            ShaderMask = 0x70,
+            // Lighting
+            Specular0 = 0x000,
+            Specular25 = 0x080,
+            Specular750 = 0x100,
+            SpecularMask = 0x180,
+            // Texture address mode
+            TextureAddressModeWrap = 0x000,
+            TextureAddressModeMirror = 0x200,
+            TextureAddressModeClamp = 0x400,
+            TextureAddressModeMask = 0x600,
+            // Night texture
+            NightTexture = 0x800,
+            // Texture to be shown in tunnels and underground (used for 3D cab night textures)
+            UndergroundTexture = 0x40000000,
+        }
+
+
+        public static string GetForestTextureFile(Simulator simulator, string textureName)
+        {
+            return GetRouteTextureFile(simulator, Helpers.TextureFlags.Spring | Helpers.TextureFlags.Autumn | Helpers.TextureFlags.Winter | Helpers.TextureFlags.SpringSnow | Helpers.TextureFlags.AutumnSnow | Helpers.TextureFlags.WinterSnow, textureName);
+        }
+
+        public static string GetNightTextureFile(Simulator simulator, string textureFilePath)
+        {
+            var texturePath = Path.GetDirectoryName(textureFilePath);
+            var textureName = Path.GetFileName(textureFilePath);
+            var nightTexturePath = !File.Exists(texturePath + @"\Night\" + textureName) ? Path.GetDirectoryName(texturePath) + @"\Night\" : texturePath + @"\Night\";
+
+            if (!String.IsNullOrEmpty(nightTexturePath + textureName) && Path.GetExtension(nightTexturePath + textureName) == ".dds" && File.Exists(nightTexturePath + textureName))
+            {
+                return nightTexturePath + textureName;
+            }
+            else if (!String.IsNullOrEmpty(nightTexturePath + textureName) && Path.GetExtension(nightTexturePath + textureName) == ".ace")
+            {
+                var alternativeTexture = Path.ChangeExtension(nightTexturePath + textureName, ".dds");
+                if (simulator.Settings.PreferDDSTexture && !String.IsNullOrEmpty(alternativeTexture.ToLower()) && File.Exists(alternativeTexture))
+                {
+                    return alternativeTexture;
+                }
+                else if (File.Exists(nightTexturePath + textureName))
+                {
+                    return nightTexturePath + textureName;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static string GetRouteTextureFile(Simulator simulator, TextureFlags textureFlags, string textureName)
+        {
+            return GetTextureFile(simulator, textureFlags, simulator.RoutePath + @"\Textures", textureName);
+        }
+
+        public static string GetTransferTextureFile(Simulator simulator, string textureName)
+        {
+            return GetTextureFile(simulator, Helpers.TextureFlags.Snow, simulator.RoutePath + @"\Textures", textureName);
+        }
+
+        public static string GetTerrainTextureFile(Simulator simulator, string textureName)
+        {
+            return GetTextureFile(simulator, Helpers.TextureFlags.Snow, simulator.RoutePath + @"\TerrTex", textureName);
+        }
+
+        public static string GetTextureFile(Simulator simulator, TextureFlags textureFlags, string texturePath, string textureName)
+        {
+            var alternativePath = @"\";
+            if ((textureFlags & TextureFlags.Snow) != 0 || (textureFlags & TextureFlags.SnowTrack) != 0)
+                if (IsSnow(simulator))
+                    alternativePath = @"\Snow\";
+                else
+                    alternativePath = @"\";
+            else if ((textureFlags & TextureFlags.Spring) != 0 && simulator.Season == SeasonType.Spring && simulator.WeatherType != WeatherType.Snow)
+                alternativePath = @"\Spring\";
+            else if ((textureFlags & TextureFlags.Autumn) != 0 && simulator.Season == SeasonType.Autumn && simulator.WeatherType != WeatherType.Snow)
+                alternativePath = @"\Autumn\";
+            else if ((textureFlags & TextureFlags.Winter) != 0 && simulator.Season == SeasonType.Winter && simulator.WeatherType != WeatherType.Snow)
+                alternativePath = @"\Winter\";
+            else if ((textureFlags & TextureFlags.SpringSnow) != 0 && simulator.Season == SeasonType.Spring && simulator.WeatherType == WeatherType.Snow)
+                alternativePath = @"\SpringSnow\";
+            else if ((textureFlags & TextureFlags.AutumnSnow) != 0 && simulator.Season == SeasonType.Autumn && simulator.WeatherType == WeatherType.Snow)
+                alternativePath = @"\AutumnSnow\";
+            else if ((textureFlags & TextureFlags.WinterSnow) != 0 && simulator.Season == SeasonType.Winter && simulator.WeatherType == WeatherType.Snow)
+                alternativePath = @"\WinterSnow\";
+
+            if (alternativePath.Length > 0) return texturePath + alternativePath + textureName;
+            return texturePath + @"\" + textureName;
+        }
+
+        public static bool IsSnow(Simulator simulator)
+        {
+            // MSTS shows snow textures:
+            //   - In winter, no matter what the weather is.
+            //   - In spring and autumn, if the weather is snow.
+            return (simulator.Season == SeasonType.Winter) || ((simulator.Season != SeasonType.Summer) && (simulator.WeatherType == WeatherType.Snow));
+        }
+
+        static readonly Dictionary<string, SceneryMaterialOptions> TextureAddressingModeNames = new Dictionary<string, SceneryMaterialOptions> {
+            { "Wrap", SceneryMaterialOptions.TextureAddressModeWrap },
+            { "Mirror", SceneryMaterialOptions.TextureAddressModeMirror },
+            { "Clamp", SceneryMaterialOptions.TextureAddressModeClamp },
+        };
+
+        static readonly Dictionary<string, SceneryMaterialOptions> ShaderNames = new Dictionary<string, SceneryMaterialOptions> {
+            { "Tex", SceneryMaterialOptions.None },
+            { "TexDiff", SceneryMaterialOptions.Diffuse },
+            { "BlendATex", SceneryMaterialOptions.AlphaBlendingBlend },
+            { "BlendATexDiff", SceneryMaterialOptions.AlphaBlendingBlend | SceneryMaterialOptions.Diffuse },
+            { "AddATex", SceneryMaterialOptions.AlphaBlendingAdd },
+            { "AddATexDiff", SceneryMaterialOptions.AlphaBlendingAdd | SceneryMaterialOptions.Diffuse },
+        };
+
+        static readonly Dictionary<string, SceneryMaterialOptions> LightingModelNames = new Dictionary<string, SceneryMaterialOptions> {
+            { "DarkShade", SceneryMaterialOptions.ShaderDarkShade },
+            { "OptHalfBright", SceneryMaterialOptions.ShaderHalfBright },
+            { "Cruciform", SceneryMaterialOptions.ShaderVegetation },
+            { "OptFullBright", SceneryMaterialOptions.ShaderFullBright },
+            { "OptSpecular750", SceneryMaterialOptions.None | SceneryMaterialOptions.Specular750 },
+            { "OptSpecular25", SceneryMaterialOptions.None | SceneryMaterialOptions.Specular25 },
+            { "OptSpecular0", SceneryMaterialOptions.None | SceneryMaterialOptions.None },
+        };
+/*
+        /// <summary>
+        /// Encodes material options code from parameterized options.
+        /// Material options encoding is documented in SharedShape.SubObject() (Shapes.cs)
+        /// or SceneryMaterial.SetState() (Materials.cs).
+        /// </summary>
+        /// <param name="lod">LODItem instance.</param>
+        /// <returns>Options code.</returns>
+        public static SceneryMaterialOptions EncodeMaterialOptions(LODItem lod)
+        {
+            var options = SceneryMaterialOptions.None;
+
+            if (TextureAddressingModeNames.ContainsKey(lod.TexAddrModeName))
+                options |= TextureAddressingModeNames[lod.TexAddrModeName];
+            else
+                Trace.TraceWarning("Skipped unknown texture addressing mode {1} in shape {0}", lod.Name, lod.TexAddrModeName);
+
+            if (lod.AlphaTestMode == 1)
+                options |= SceneryMaterialOptions.AlphaTest;
+
+            if (ShaderNames.ContainsKey(lod.ShaderName))
+                options |= ShaderNames[lod.ShaderName];
+            else
+                Trace.TraceWarning("Skipped unknown shader name {1} in shape {0}", lod.Name, lod.ShaderName);
+
+            if (LightingModelNames.ContainsKey(lod.LightModelName))
+                options |= LightingModelNames[lod.LightModelName];
+            else
+                Trace.TraceWarning("Skipped unknown lighting model index {1} in shape {0}", lod.Name, lod.LightModelName);
+
+            if ((lod.ESD_Alternative_Texture & (int)TextureFlags.Night) != 0)
+                options |= SceneryMaterialOptions.NightTexture;
+
+            return options;
+        } // end EncodeMaterialOptions
+        */
+    } // end class Helpers
+}
--- ShapeViewer/ShapeViewer.cs
+++ ShapeViewer/ShapeViewer.cs
@@ -19,7 +19,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Orts.Formats.Msts;
-using System.Collections.Generic;
 
 namespace ShapeViewer
 {
@@ -41,11 +40,10 @@ namespace ShapeViewer
         //BasicEffect for rendering
         BasicEffect basicEffect;
 
-        //Geometric info
-        VertexPositionColor[] triangleVertices;
-        VertexBuffer vertexBuffer;
-        IndexBuffer indexBuffer;
-        List<int> indices;
+        Viewer viewer;
+        SharedShape sh;
+
+        string examples = "..\\ORTools\\ShapeViewer\\samples\\";
 
         public ShapeViewer()
         {
@@ -81,9 +79,6 @@ namespace ShapeViewer
             basicEffect = new BasicEffect(GraphicsDevice);
             basicEffect.Alpha = 1f;
 
-            // Want to see the colors of the vertices, this needs to be on
-            basicEffect.VertexColorEnabled = true;
-
             //Lighting requires normal information which VertexPositionColor does not have
             //If you want to use lighting and VPC you need to create a custom def
             basicEffect.LightingEnabled = false;
@@ -98,39 +93,10 @@ namespace ShapeViewer
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            viewer = new Viewer();
+            viewer.GraphicsDevice = GraphicsDevice;
+            sh = new SharedShape(viewer, examples + "SHAPES\\TSF_MAR_FV_Pietracuta.s");
 
-            // TODO: use this.Content to load your game content here
-            ShapeFile shape = new ShapeFile("..\\ORTools\\ShapeViewer\\samples\\SHAPES\\TSF_MAR_FV_Pietracuta.s", true);
-
-            VertexPositionColor[] vertices = new VertexPositionColor[shape.shape.points.Count];
-            for(int j=0; j < shape.shape.points.Count; j++)
-            {
-                vertices[j] = new VertexPositionColor(
-                    new Vector3(shape.shape.points[j].X, shape.shape.points[j].Y, shape.shape.points[j].Z),
-                    Color.Yellow);
-            }
-            // Set up the vertex buffer
-            vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor), 
-                shape.shape.points.Count, BufferUsage.WriteOnly);
-            vertexBuffer.SetData<VertexPositionColor>(vertices);
-
-            // vertex indexes
-            indices = new List<int>();
-            sub_objects subs = shape.shape.lod_controls[0].distance_levels[0].sub_objects;
-            foreach(var so in subs)
-            {
-                foreach(var prim in so.primitives)
-                {
-                    foreach(var vi in prim.indexed_trilist.vertex_idxs)
-                    {
-                        indices.Add(vi.a);
-                        indices.Add(vi.b);
-                        indices.Add(vi.c);
-                    }
-                }
-            }
-            indexBuffer = new IndexBuffer(graphics.GraphicsDevice, IndexElementSize.ThirtyTwoBits, indices.Count, BufferUsage.WriteOnly);
-            indexBuffer.SetData(indices.ToArray());
         }
 
         /// <summary>
@@ -166,22 +132,32 @@ namespace ShapeViewer
             basicEffect.Projection = projectionMatrix;
             basicEffect.View = viewMatrix;
             basicEffect.World = worldMatrix;
-            basicEffect.VertexColorEnabled = true;
 
-            GraphicsDevice.SetVertexBuffer(vertexBuffer);
-            GraphicsDevice.Indices = indexBuffer;
-
+            Texture2D Texture = AceFile.Texture2DFromFile(GraphicsDevice, examples + "TEXTURES\\TSF_MAR_St_Pietracuta.ace");
+            basicEffect.Texture = Texture;
+            basicEffect.TextureEnabled = true;
             RasterizerState rasterizerState = new RasterizerState();
             rasterizerState.CullMode = CullMode.None;
             GraphicsDevice.RasterizerState = rasterizerState;
 
+            SharedShape.SubObject[] subs = sh.LodControls[0].DistanceLevels[0].SubObjects;
             foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
             {
                 pass.Apply();
-                GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, indices.Count);
+                foreach (var so  in subs)
+                {
+                    foreach (var pr in so.ShapePrimitives)
+                    {
+                        pr.Draw(GraphicsDevice);
+                    }
+                }
             }
+
+
 
             base.Draw(gameTime);
         }
     }
+
+
 }
